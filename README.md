063548b2f2e7b87633f6458b8d6613a682e9ac4d
tuanpembual <andro.medh4@gmail.com>
2014-07-17 13:26:16 +0700

    parsing json static

--- README.md
+++ README.md
@@ -2,16 +2,23 @@ Welcome to Onsen UI
 
 Here is how to run the project
 
-1 Install nodejs
+1. Install nodejs
 
 	http://nodejs.org/download/
 	
-2 Run the project
+2. Run the project
 
 type: 
 	
 		node scripts/web-server.js
 
-3 Open the url
+3. Open the url
 
 		http://localhost:8000/app/index.html
+
+4. Filter posting base on category,(sample : desain)
+		
+		http://panduan.blankonlinux.or.id/?json=get_category_posts&slug=desain&count=10&status=publish
+
+5. How show json to html ?
+hack core!!!
--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,21 @@
-(function(){
-	'use strict';
-	angular.module('myApp', ['onsen.directives']);
-})();
+'use strict';
+
+/* App Module
+Change
+phonecatApp => PanduanApp
+phonecatControllers => PanduanCtr
+*/
+
+var myApp = angular.module('myApp', [
+  'onsen.directives',
+  //'PanduanCtr',
+]);
+
+// var PanduanCtr = angular.module('PanduanCtr', []);
+
+myApp.controller('ShowCtr', function($scope, $http) {
+  $http.get('multimedia.json')
+       .then(function(res){
+          $scope.todos = res.data;                
+        });
+});
